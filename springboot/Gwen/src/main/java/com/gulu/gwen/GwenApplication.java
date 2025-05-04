ffac1ff580135eff05a1232e73af6a451be8d9ef
Gwen <Gwen@macbook.com>
2025-05-05 03:55:11 +0800

    后端初始化

---
+++ springboot/Gwen/src/main/java/com/gulu/gwen/GwenApplication.java
@@ -0,0 +1,13 @@
+package com.gulu.gwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwenApplication.class, args);
+    }
+
+}
---
+++ springboot/Gwen/src/test/java/com/gulu/gwen/GwenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulu.gwen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
